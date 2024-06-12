6330c8a0c732ff6d4178148cc6ba623a484409d1
Daniel Martinez <danmc018@gmail.com>
2024-06-12 21:02:00 +0200

    Practica 1 - Ejercicios 1-5

---
+++ Ejercicios/Practica1/Ejercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ejercicio3
+{
+    using System;
+
+    class ejercicio3
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            int resultado;
+            Console.WriteLine("######## TERNA PITAGORICA ########");
+            
+            Console.Write("Introduce el primer número: ");
+            a = int.Parse(Console.ReadLine().Trim());
+            Console.Write("Introduce el segundo número: ");
+            b = int.Parse(Console.ReadLine().Trim());
+            Console.Write("Introduce el tercer número: ");
+            c = int.Parse(Console.ReadLine().Trim());
+            //Convert.ToInt32( Console.ReadLine() )
+            
+            Console.WriteLine((a*a)+" + "+(b*b)+ " = " +(c*c));
+            if ((a * a) + (b * b) == (c * c))
+            {
+                Console.WriteLine("\nEs una terna pitagorica");
+            }
+            else
+            {
+                Console.WriteLine("\nNo es una terna pitagorica");
+            }
+        }
+    }
+    
+}
+
---
+++ Ejercicios/Practica1/Ejercicio4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ejercicio4
+{
+    using System;
+
+    class ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            int aux;
+            int ant = 0;
+            int act = 1;
+            int numeroPosiciones;
+            string respuesta;
+            int[] fibo;
+            Console.WriteLine("######### SUCESION FIBONACHI #########");
+
+            do
+            {
+                Console.Write("Numero de elementos?: ");
+                respuesta = Console.ReadLine().Trim();
+            } while (!(int.TryParse(respuesta, out numeroPosiciones)) || numeroPosiciones<=0);
+
+            for (int i = 1; i <= numeroPosiciones; i++)
+            {
+                
+                aux = ant + act;
+                if (i >= 2)
+                {
+                    ant = act;
+                    act = aux;
+                }
+
+                
+                Console.Write(aux);
+                if (i != numeroPosiciones)
+                {
+                    Console.Write(", ");
+                }
+
+            }
+        }
+    }
+}
